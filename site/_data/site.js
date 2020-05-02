581750af72d45f8dcde27dbf2295a762cb5a61a6
Sami Singh <sami@httpster.io>
2020-05-03 00:04:26 +0300

    initial commit

---
+++ site/_data/site.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: "Supermaya",
+  shortDesc:
+    "An 11ty starter kit designed to help you add rich features to a site without a complicated build process.",
+  url: "",
+  authorEmail: "mike@madebymike.com.au",
+  authorHandle: "@MikeRiethmuller",
+  authorName: "Mike",
+  postsPerPage: 4,
+  socialImage: "/img/social.jpg",
+  theme: {
+    primary: {
+      background: "white",
+      text: "black",
+      highlight: "#666",
+    },
+    secondary: {
+      background: "white",
+      text: "black",
+      highlight: "#666",
+    },
+  },
+
+  // Critical CSS results in much slower build times and uses a lot of system resources
+  // turn on in production :)
+  // See `site/transforms/critical-css-transform.js` for more details
+  criticalCSS: false,
+};
---
+++ site/index.md
@@ -0,0 +1,18 @@
+---
+layout: "layouts/home.njk"
+title: Simple Blog
+postsHeading: Latest posts
+allPostsText: See all posts
+socialImage: ""
+---
+
+An 11ty starter kit designed to help you add rich features to a site without a complicated build process. Testing
+
+<ul>
+    <li>
+        <a href="#">Deploy Supermaya</a> <small>(add user-generated content)</small>
+    </li>
+    <li>
+        <a href="https://app.netlify.com/start/deploy?repository=https://github.com/MadeByMike/supermaya">Deploy Supermaya to Netlify</a>
+    </li>
+</ul>

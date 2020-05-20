aa0900855ec7e4cacd526bb5d85451f2361ac739
Sami Singh <sami@httpster.io>
2020-05-21 02:56:52 +0300

    aaaaa

--- site/_data/js.js
+++ site/_data/js.js
@@ -7,7 +7,7 @@ const { compileWebpackTargets } = require("../utils/compile-webpack");
 // Add Webpack compilation targets here
 const targets = {
   main: "site/src/js/main.js",
-  "service-worker": "site/src/service-worker.js"
+  "service-worker": "site/static/service-worker.js"
 };
 
 // Create an object with the compiled files from each entry defined in targets

5c1dac42143646b96ac9db4a874ddd7b8cbd21f4
Sami Singh <sami@httpster.io>
2020-05-21 02:48:04 +0300

    howbout now?

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -6,7 +6,7 @@ const butInstall = document.getElementById('butInstall');
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/js/service-worker.js');
+  navigator.serviceWorker.register('/service-worker.js');
 }
 
 /**

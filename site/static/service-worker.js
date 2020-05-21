fb054b501f6a0fc0dfd25fc2e747c10bc2526c64
Sami Singh <sami@httpster.io>
2020-05-21 03:37:45 +0300

    fucking jesus christ

--- site/static/service-worker.js
+++ site/static/service-worker.js
@@ -1,99 +1,32 @@
-const CACHE_KEY = `httpster-1`;
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
+  'index.html',
+  'css/main.css',
+];
 
-// URLS that we don’t want to end up in the cache
-const EXCLUDED_URLS = [];
-
-// URLS that we want to be cached when the worker is installed
-const PRE_CACHE = ["/", "/posts", "/fonts/Inter-roman.var-subset.woff2?v=3.13"];
-
-// You might want to bypass a certain host
-const IGNORED_HOSTS = [];
-
-const divInstall = self.getElementById('installContainer');
-const butInstall = self.getElementById('butInstall');
-
-const addToCache = function (items) {
-  caches.open(CACHE_KEY).then(cache => cache.addAll(items));
-};
-
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", evt => {
-  // Look for any old caches that don't match our set and clear them out
-  evt.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(
-          item => !Object.values(CACHE_KEY).includes(item)
-        );
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
       })
-      .then(itemsToDelete => {
-        return Promise.all(
-          itemsToDelete.map(item => {
-            return caches.delete(item);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", event => {
-  // Ignore hosts
-  const { hostname } = new URL(event.request.url);
-  event.respondWith(fetch(event.request));
-  if (IGNORED_HOSTS.indexOf(hostname) >= 0) {
-    return;
-  }
-
-  // Ignore URLs
-  if (EXCLUDED_URLS.some(page => event.request.url.indexOf(page) > -1)) {
-    return;
-  }
-
-  // Respond with cache first but complete network request and update cache if possible
-  return caches.open(CACHE_KEY).then(cache => {
-    return cache.match(event.request).then(response => {
-      var fetchPromise = fetch(event.request).then(networkResponse => {
-        cache.put(event.request, networkResponse.clone());
-        return networkResponse;
-      });
-      return response || fetchPromise;
-    });
-  });
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
 });
 
-self.addEventListener('beforeinstallprompt', (event) => {
-  console.log('👍', 'beforeinstallprompt', event);
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container
-  divInstall.classList.toggle('hidden', false);
-});
-
-self.addEventListener('click', () => {
-  console.log('👍', 'butInstall-clicked');
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    // The deferred prompt isn't available.
-    return;
-  }
-  // Show the install prompt.
-  promptEvent.prompt();
-  // Log the result
-  promptEvent.userChoice.then((result) => {
-    console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
-    divInstall.classList.toggle('hidden', true);
-  });
-});
-
-self.addEventListener('appinstalled', (event) => {
-  console.log('👍', 'appinstalled', event);
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
 });

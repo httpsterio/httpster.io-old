db95a9eac7f55889cd45003b41f7f41c82848209
Sami Singh <sami@httpster.io>
2020-05-30 04:19:23 +0300

    service worker button update

--- site/static/service-worker.js
+++ site/static/service-worker.js
@@ -3,15 +3,17 @@ const precacheResources = [
   '/',
   'index.html',
   'css/main.css',
+  'fonts/Inter-roman.var-subset.woff2?v=3.13',
+  'blog/'
 ];
 
 self.addEventListener('install', event => {
   console.log('Service worker install event!');
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(precacheResources);
-      })
+    .then(cache => {
+      return cache.addAll(precacheResources);
+    })
   );
 });
 

f5701189d6df06f8b943e44e68eef763222dd7fd
Sami Mäkelä <sami@httpster.io>
2022-09-11 20:28:56 +0300

    fixed rendering engine to njk

--- .eleventy.js
+++ .eleventy.js
@@ -92,6 +92,21 @@ module.exports = function (config) {
   }
 
   return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+    
     dir: {
       input: "site",
       output: "dist"
